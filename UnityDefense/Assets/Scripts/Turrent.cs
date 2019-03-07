6b1ce73d9a7c407d1c79ff6e3b546909d2294adc
dror <sametcoh@gmail.com>
2019-03-07 12:59:48 +0200

    turrent rotation + wave spawn

---
+++ UnityDefense/Assets/Scripts/Turrent.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turrent : MonoBehaviour {
+
+    private Transform target;
+    public float range = 5f;
+
+    public string enemyTag = "Enemy";
+
+    public Transform partToRotate;
+    public float turnSpeed = 10f;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (target == null)
+            return;
+
+        LookAtTargetDirection();
+        
+	}
+
+    private void LookAtTargetDirection()
+    {
+        // TODO: rotate on x/z axis also!
+        Vector3 direction = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+        partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+    }
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+
+            if(distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+
+            if(nearestEnemy != null && shortestDistance <= range)
+            {
+                target = nearestEnemy.transform;
+            } else
+            {
+                target = null;
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
---
+++ UnityDefense/Assets/WaveSpawner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour {
+
+    public Transform enemyPrefab;
+
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5f;
+    private float countdown = 2f;
+
+    private int waveNumber = 1;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+	}
+
+    IEnumerator SpawnWave()
+    {
+        for (int i = 0; i < waveNumber; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+
+        waveNumber++;
+    }
+
+    private void SpawnEnemy()
+    {
+        Vector3 spawnPositionXZ = new Vector3(spawnPoint.position.x, 0.5f, spawnPoint.position.z);
+        Instantiate(enemyPrefab, spawnPositionXZ, spawnPoint.rotation);
+    }
+
+}
